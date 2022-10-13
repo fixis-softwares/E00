07753296b01d20bcb4738fbabe19b091c717ca29
fixis-softwares-E00 <github-e00@fixis.fr>
2022-10-13 14:00:10 +0200

    message supplémentaire

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 public class Test {
 
     public static void main(String[] args) {
+
+        System.out.println("Test merge");
         System.out.println(LocalDate.now());
     }
 }
