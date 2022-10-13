b552f798adbe8f21c4ac25dbc1dc9133aad33d9d
François-Xavier SENNESAL <francois-xavier.sennesal@inrae.fr>
2022-10-13 13:57:39 +0200

    Ajout d'un message

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("Date actuelle: ");
         System.out.println(LocalDate.now());
     }
 }
