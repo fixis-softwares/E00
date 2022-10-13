0f7f17216e42adb6245e6712ca2eae9566ec6267
fixis-softwares-E00 <github-e00@fixis.fr>
2022-10-13 14:09:25 +0200

    message supplémentaire

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Test merge");
+        System.out.println("Date actuelle: ");
         System.out.println(LocalDate.now());
     }
 }
